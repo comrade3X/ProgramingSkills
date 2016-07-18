6b18e2ef32988c0d0019ee62251bf3b455a55b64
comrade3X <kidbandesit@gmail.com>
2016-07-18 22:46:45 +0700

    Update Stack, Queue example

---
+++ ProgrammingBasicsSkill/Queue/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Queue:First in First out (FIFO)
+            //Collection, Enumerable
+            //Cac tinh chat con lai tuong tu Stack
+            Queue<int> queue = new Queue<int>(3);
+            for (int i = 0; i < 10; i++)
+            {
+                queue.Enqueue(i);
+            }
+
+            //In ra phan tu dau tien cua Queue (ko xoa)
+            Console.WriteLine(queue.Peek());
+
+            //xoa toan bo phan tu cua Queue
+            //queue.Clear();
+
+            while (queue.Count > 0)
+            {
+                Console.Write(queue.Dequeue() + " ");
+            }   
+            Console.WriteLine("So phan tu cua Queue sau khi Dequeue: {0}", queue.Count);
+          
+            Console.ReadLine();
+        }
+    }
+}
--- ProgrammingBasicsSkill/StackQueu/Program.cs
+++ ProgrammingBasicsSkill/StackQueu/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace StackQueu
+namespace Stack
 {
     class Program
     {
@@ -14,16 +14,28 @@ namespace StackQueu
             //Collection, Enumerable
             Stack<int> stack = new Stack<int>();
 
+            // Dua phan tu vao stack
             for (int i = 0; i < 10; i++)
             {
                 stack.Push(i);
             }
+            Console.WriteLine("So phan tu cua stack: {0}",stack.Count);
 
+            //In ra man hinh cac phan tu cua stack (dont remove)
             foreach (var item in stack)
             {
-                Console.Write(item + " ");
+                Console.Write(item+" ");
             }
+            Console.WriteLine("So phan tu cua stack: {0}", stack.Count);
 
+
+            //Xuat ra cac phan tu cua stack su dung method pop() (remove after pop)
+            while (stack.Count > 0)
+            {
+                Console.Write(stack.Pop() + " ");
+            }
+            Console.WriteLine("So phan tu cua stack sau khi xuat ra(pop): {0}", stack.Count);
+            
             Console.ReadLine();
         }
     }
