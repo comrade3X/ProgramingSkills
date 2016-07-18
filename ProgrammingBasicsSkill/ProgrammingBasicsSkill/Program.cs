1194e7476d5f96a2963ba96bbc5dbc3af1983420
comrade3X <kidbandesit@gmail.com>
2016-07-18 14:58:17 +0700

    Add new

---
+++ ProgrammingBasicsSkill/ProgrammingBasicsSkill/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingBasicsSkill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 3;
+            var b = 5;
+            var c = a & b;
+            var str1 = "dung";
+            var sum = str1 + 100;
+            Console.Write(sum);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ ProgrammingBasicsSkill/StackQueu/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackQueu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Stack: Last in First out (LIFO)
+            //Collection, Enumerable
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                stack.Push(i);
+            }
+
+            foreach (var item in stack)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
